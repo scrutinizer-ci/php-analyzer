7f2d612b1e63093e099d0b0c7bf7ca10bde9a9b3
siad007 <siad.ardroumli@gmail.com>
2014-03-29 21:54:40 +0100

    added stubs for uploadprogress

---
+++ res/php-5.2-core-api/uploadprogress.php
@@ -0,0 +1,12 @@
+<?php
+
+// Start of uploadprogress v.1.0.3.1
+
+/** @jms-builtin */
+function uploadprogress_get_info () {}
+
+/** @jms-builtin */
+function uploadprogress_get_contents () {}
+
+// End of uploadprogress v.1.0.3.1
+?>
